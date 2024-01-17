a05689f8e6c341218e3e849e683e5ea447ba5f50
Giovanny Padilla <giovanny.padilla@udla.edu.ec>
2024-01-17 18:30:36 -0500

    Entrada y salida de datos para ingresar la informacion del alumno

--- Estruc.c
+++ Estruc.c
@@ -13,3 +13,29 @@ struct Alumno
 //funcion para los datos y los resultados
 void Datos(struct Alumno *alumno);
 void Resultados(struct Alumno *alumno);
+
+int main()
+{
+    int num; // Número de alumnos
+    printf("Indique la cantidad de alumnos que desea ingresar:\n ");
+    scanf("%d", &num);
+    fflush(stdin); // Limpiar el búfer 
+
+    struct Alumno estudiante[num]; // estructura para almacenar los datos de los alumnos
+
+    // uso del for para ingresar los datos de cada alumno
+    for (int i = 0; i < num; i++)
+    {
+        printf("\nIngrese los datos del alumno %d:\n", i + 1);
+        Datos(&estudiante[i]);
+    }
+
+    // bucle para mostrar los datos de cada alumno
+    for (int i = 0; i < num; i++)
+    {
+        printf("\nDatos del alumno %d:\n", i + 1);
+        Resultados(&estudiante[i]);
+    }
+
+    return 0;
+}
